77e7fc6be9eef200a12049a528c599890c4a61c8
Ivan E. Batista-Ochoa <ivan.batista@mercadolibre.com.co>
2022-08-12 11:49:41 -0500

    Hydrate page at / (client)

---
+++ app/client/home.js
@@ -0,0 +1,19 @@
+const React = require("react");
+const hydrate = require("nordic/hydrate");
+const I18n = require("nordic/i18n");
+const I18nProvider = require("nordic/i18n/I18nProvider");
+const ImageProvider = require("nordic/image/provider");
+const HomeView = require("../pages/home/view");
+
+const { products, translations, imagesPrefix } = window.__PRELOADED_STATE__;
+
+const i18n = new I18n({ translations });
+
+hydrate(
+  <I18nProvider i18n={i18n}>
+    <ImageProvider prefix={imagesPrefix}>
+      <HomeView products={products} />
+    </ImageProvider>
+  </I18nProvider>,
+  document.getElementById("root-app")
+);
