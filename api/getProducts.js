b567b1cc138fa3d700b08ba73892420c6257b033
Ivan E. Batista-Ochoa <ivan.batista@mercadolibre.com.co>
2022-08-12 11:47:54 -0500

    Add endpoint /getProducts to our api

---
+++ api/getProducts.js
@@ -0,0 +1,16 @@
+const router = require("nordic/ragnar").router();
+const ProductService = require("../services/productService");
+
+router.get("/", (req, res) => {
+  const { name, limit, offset } = req.query;
+  const { siteId } = req.platform;
+
+  ProductService.getProducts(siteId, name, limit, offset)
+    .then((response) => res.status(200).json(response))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json([]);
+    });
+});
+
+module.exports = router;
