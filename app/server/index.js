4cbd921e821e24d42319ce9995ce9a3de5b783cc
Ivan E. Batista-Ochoa <ivan.batista@mercadolibre.com.co>
2022-08-12 11:46:12 -0500

    Add app route to index.js in server /

--- app/server/index.js
+++ app/server/index.js
@@ -1,21 +1,22 @@
 /**
  * Module dependencies
  */
-const router = require('nordic/ragnar').router();
-const config = require('nordic/config');
-const { layoutMiddleware } = require('nordic/layout');
-const i18nMiddleware = require('nordic/i18n/middleware');
-const polyfillsMiddleware = require('nordic/script/polyfills-middleware');
+const router = require("nordic/ragnar").router();
+const config = require("nordic/config");
+const { layoutMiddleware } = require("nordic/layout");
+const i18nMiddleware = require("nordic/i18n/middleware");
+const polyfillsMiddleware = require("nordic/script/polyfills-middleware");
 
 /**
  * Set up mocks
  */
-require('../../mocks');
+require("../../mocks");
 
 /**
  * Routers
  */
-const demoRoute = require('../pages/demo');
+const demoRoute = require("../pages/demo");
+const homeRoute = require("../pages/home");
 
 /**
  * Use global middlewares
@@ -27,12 +28,13 @@ router.use(polyfillsMiddleware(config.polyfillLimits));
 /**
  * Redirect
  */
-router.get('/', (req, res) => res.redirect(`${config.ragnar.basePath}demo`));
+// router.get('/', (req, res) => res.redirect(`${config.ragnar.basePath}demo`));
 
 /**
  * Mount routers
  */
-router.use('/demo', demoRoute);
+router.use("/", homeRoute);
+router.use("/demo", demoRoute);
 
 /**
  * Expose router
