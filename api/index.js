652ab85d5bcd80a0e517f00934a9fbf6b74b5bf1
Ivan E. Batista-Ochoa <ivan.batista@mercadolibre.com.co>
2022-08-12 11:44:40 -0500

    Add getProducts route in our api

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 /**
  * Module dependencies
  */
-const router = require('nordic/ragnar').router();
-const demo = require('./demo');
+const router = require("nordic/ragnar").router();
+const demo = require("./demo");
+const getProducts = require("./getProducts");
 
 /**
  * Demo router
  */
-router.use('/demo', demo);
+router.use("/demo", demo);
+router.use("/getProducts", getProducts);
 
 /**
  * Expose API router
