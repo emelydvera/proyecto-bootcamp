8397f6e77dadee4571f9bfbf1c12599a33719e6e
Ivan E. Batista-Ochoa <ivan.batista@mercadolibre.com.co>
2022-08-13 09:14:17 -0500

    Refactor Filter component to show error and refine search

--- app/components/Filter/Filter.js
+++ app/components/Filter/Filter.js
@@ -1,28 +1,54 @@
 const React = require("react");
+const { useState } = React;
 
 const Filter = ({ products, setProductsState, i18n }) => {
+  const [error, setError] = useState("");
+
   const handleFilterProducts = (e) => {
     e.preventDefault();
     const [min, max] = e.target;
-    setProductsState(() =>
-      products.filter((p) => p.price >= min.value && p.price <= max.value)
-    );
+
+    if (!min.value && !max.value) {
+      setError(() => "Diligencie alguno de los campos");
+    } else if (min.value && max.value) {
+      setError(() => "");
+      setProductsState(() =>
+        products.filter(
+          (p) =>
+            p.price >= parseInt(min.value) && p.price <= parseInt(max.value)
+        )
+      );
+    } else if (min.value && !max.value) {
+      console.log(min.value);
+      setError(() => "");
+      setProductsState(() =>
+        products.filter((p) => p.price >= parseInt(min.value))
+      );
+    } else if (max.value && !min.value) {
+      setError(() => "");
+      setProductsState(() =>
+        products.filter((p) => p.price <= parseInt(max.value))
+      );
+    }
   };
 
   return (
-    <form onSubmit={handleFilterProducts}>
-      <input
-        type="number"
-        placeholder={i18n.gettext("minimo")}
-        aria-label={i18n.gettext("Escriba el precio minimo")}
-      />
-      <input
-        type="number"
-        placeholder={i18n.gettext("maximo")}
-        aria-label={i18n.gettext("Escriba el precio maximo")}
-      />
-      <input type="submit" value={i18n.gettext("Filtrar")} />
-    </form>
+    <>
+      <form onSubmit={handleFilterProducts}>
+        <input
+          type="number"
+          placeholder={i18n.gettext("mínimo")}
+          aria-label={i18n.gettext("Escriba el precio minimo")}
+        />
+        <input
+          type="number"
+          placeholder={i18n.gettext("máximo")}
+          aria-label={i18n.gettext("Escriba el precio maximo")}
+        />
+        <input type="submit" value={i18n.gettext("Filtrar")} />
+      </form>
+      <p>{error}</p>
+    </>
   );
 };
 
