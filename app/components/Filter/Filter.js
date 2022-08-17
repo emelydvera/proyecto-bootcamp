b7cb95c9ca56970e7c5e3a368e0a00abe9257a12
GabrielaDanaP <gabriela.pacheco@mercadolibre.cl>
2022-08-17 13:00:12 -0400

    fix name component proptype

--- app/components/Filter/Filter.js
+++ app/components/Filter/Filter.js
@@ -58,7 +58,7 @@ const Filter = ({ products, setProductsState, i18n }) => {
   );
 };
 
-ProductsList.proptypes = {
+Filter.proptypes = {
   products: PropTypes.arrayOf(PropTypes.object).isRequired,
   setProducts: PropTypes.func.isRequired,
   i18n: PropTypes.shape({
