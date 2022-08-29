e50085fd1cdd36d229f3f64c0883e2937ae0c536
GabrielaDanaP <gabriela.pacheco@mercadolibre.cl>
2022-08-29 14:50:14 -0400

    add proptypes page error404

--- app/pages/error404/view.js
+++ app/pages/error404/view.js
@@ -3,6 +3,7 @@ const Script = require("nordic/script");
 const serialize = require("serialize-javascript");
 const { injectI18n } = require("nordic/i18n");
 const Component404 = require("../../components/Component404");
+const PropTypes = require('prop-types');
 
 function View(props) {
   const { i18n, translations, imagesPrefix } = props;
@@ -26,6 +27,14 @@ function View(props) {
       <Component404 i18n={i18n} />
     </>
   );
+}
+
+View.PropTypes = {
+  i18n: PropTypes.shape({
+    gettext: PropTypes.func.isRequired,
+  }).isRequired,
+  translations: PropTypes.shape({}),
+  imagesPrefix: PropTypes.string,
 }
 
 module.exports = injectI18n(View);
