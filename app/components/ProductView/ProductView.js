1cc99ce7f97bc49d4854bb1a2f4e8ed5357834df
emelydvera <emely.vera@mercadolibre.com.co>
2022-08-31 16:26:50 -0500

    Add styles productView

--- app/components/ProductView/ProductView.js
+++ app/components/ProductView/ProductView.js
@@ -33,28 +33,36 @@ const ProductView = ({ product, i18n, description, quantity, amount }) => {
     <section >
 
       <div className="product">
-        <div className="product-img">
-          <div className="product-img-carousel">
+        <div className="product__img">
+          <div className="product__img__carousel">
             {
               pictures.length > 0 &&
               pictures.map((p, i) => {
                 return (
 
-                  i <= 6 && <Image
-                    className='product__image'
-                    key={p.id}
-                    onMouseOver={() => { handleOver(i) }}
-                    aria-label={i18n.gettext("imagen del producto")}
-                    src={p.secure_url}
-                    alt={title}
-                  />
+                  i <= 6 &&
+                  <div className={`image__container ${mainImage === i ? 'active' : ""}`}>
+                    <Image
+                      className='product__image '
+                      key={p.id}
+                      onMouseOver={() => { handleOver(i) }}
+                      aria-label={i18n.gettext("imagen del producto")}
+                      src={p.secure_url}
+                      alt={title}
+                    />
+                  </div>
                 )
               })
             }
           </div>
 
           <figure tabIndex={9} aria-label={i18n.gettext(` imagen de ${title}`)}>
-            <Image src={pictures[mainImage].secure_url} alt={i18n.gettext("producto")} lazyload={true} className='product-img-main' />
+            <Image
+              src={pictures[mainImage].secure_url}
+              alt={i18n.gettext("producto")}
+              lazyload={true}
+              className='product-img-main'
+            />
             <figcaption>{i18n.gettext(title)}</figcaption>
           </figure>
         </div>
@@ -62,7 +70,7 @@ const ProductView = ({ product, i18n, description, quantity, amount }) => {
         <p tabIndex={18}>{i18n.gettext(description.plain_text)}</p>
       </div>
 
-      <div>
+      <div className="info">
 
         <span tabIndex={10} aria-label={`${condition}`}>{i18n.gettext(
           condition === 'new'
@@ -71,14 +79,14 @@ const ProductView = ({ product, i18n, description, quantity, amount }) => {
               ? 'No Especifica' :
               condition === 'used'
                 ? 'Usado' : ''
-        )} -
+        )}
         </span>
-        <span tabIndex={11} aria-label={i18n.gettext(`${sold_quantity} productos vendidos`)}> {i18n.gettext(`${sold_quantity} vendidos`)}</span>
+        <span tabIndex={11} aria-label={i18n.gettext(`${sold_quantity} productos vendidos`)}> {i18n.gettext(`| ${sold_quantity} vendidos`)}</span>
         <h3 tabIndex={12} aria-label={i18n.gettext(`${title}`)}>{i18n.gettext(title)}</h3>
-        <h2 tabIndex={13} aria-label={i18n.gettext(`el producto tiene un precio de ${price}`)}>{i18n.gettext(`$ ${price}`)}</h2>
+        <h2 tabIndex={13} aria-label={i18n.gettext(`el producto tiene un precio de ${price}`)}>{i18n.gettext(`$ ${new Intl.NumberFormat().format(price)}`)}</h2>
 
         {!quantity || !amount ? null : (
-          <p tabIndex={14}>{i18n.gettext(`cuotas: ${quantity} de ${amount} cada cuota`)}</p>
+          <p tabIndex={14}>{i18n.gettext(` ${quantity} cuotas de $ ${new Intl.NumberFormat().format(amount)} cada una`)}</p>
         )}
 
 
@@ -89,24 +97,27 @@ const ProductView = ({ product, i18n, description, quantity, amount }) => {
           }
         </p>
 
+        <div className="buy__container">
+          <InputQuantity
+            tabIndex={19}
+            availableQuantity={available_quantity}
+            setError={setError}
+            setQuantityToBuy={setQuantityToBuy}
+            quantityToBuy={quantityToBuy}
+          />
+          {<span className="error">{error && error}</span>}
+          <button
+            tabIndex={20}
+            onClick={() => handleClick(id, quantityToBuy)}
+            type="submit"
+            disabled={error}
+            aria-label={i18n.gettext("botón para comprar producto")}
+          >
+            {i18n.gettext("Comprar")}
+          </button>
 
-        <InputQuantity
-          tabIndex={19}
-          availableQuantity={available_quantity}
-          setError={setError}
-          setQuantityToBuy={setQuantityToBuy}
-          quantityToBuy={quantityToBuy}
-        />
-        {error ? <span>{error}</span> : null}
-        <button
-          tabIndex={20}
-          onClick={() => handleClick(id, quantityToBuy)}
-          type="submit"
-          disabled={error}
-          aria-label={i18n.gettext("botón para comprar producto")}
-        >
-          {i18n.gettext("Comprar")}
-        </button>
+        </div>
+
       </div>
 
     </section>
