f08bdf84a84710f2bbad9d9a404a9aa12c725ccd
Ivan E. Batista-Ochoa <ivan.batista@mercadolibre.com.co>
2022-09-01 17:53:08 -0500

    Add i18n, aria-label and PropTypes to SelectProductsPerPage component

--- app/components/SelectProductsPerPage/SelectProductPerPage.js
+++ app/components/SelectProductsPerPage/SelectProductPerPage.js
@@ -1,16 +1,30 @@
 const React = require("react");
-const Image = require("nordic/image");
+const PropTypes = require("prop-types");
+const { useI18n } = require("nordic/i18n");
 
 const options = [{ value: 5 }, { value: 10 }, { value: 15 }, { value: 20 }];
 
 const SelectProductsPerPage = ({ setLimit, limit }) => {
+  const { i18n } = useI18n();
   const handleChange = (e) => {
     setLimit(parseInt(e.target.value));
   };
   return (
     <div className="products__per-page">
-      <span>Productos por página</span>
+      <label
+        aria-label={i18n.gettext(
+          "Está página está mostrando {0} productos",
+          limit
+        )}
+        tabIndex={9}
+      >
+        Productos por página
+      </label>
       <select
+        aria-label={i18n.gettext(
+          "Cambia el número de productos mostrados por la página"
+        )}
+        tabIndex={9}
         className="products__per-page__select"
         onChange={handleChange}
         defaultValue={limit.toString()}
@@ -21,13 +35,13 @@ const SelectProductsPerPage = ({ setLimit, limit }) => {
           </option>
         ))}
       </select>
-      {/* <Image
-        src="dropdown-image.svg"
-        // lazyload="off"
-        className="products__per-page__dropdown"
-      ></Image> */}
     </div>
   );
+};
+
+SelectProductsPerPage.propTypes = {
+  setLimit: PropTypes.func.isRequired,
+  limit: PropTypes.number.isRequired,
 };
 
 module.exports = SelectProductsPerPage;
