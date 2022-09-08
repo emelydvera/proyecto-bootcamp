f2618575075898325ab85f10099f91c1bb23fb6a
Ivan E. Batista-Ochoa <ivan.batista@mercadolibre.com.co>
2022-09-08 09:57:52 -0500

    Add Card & CardContent components and adjust layout according to figma

--- app/components/ProductsCard/ProductsCard.js
+++ app/components/ProductsCard/ProductsCard.js
@@ -1,54 +1,61 @@
 const React = require("react");
 const PropTypes = require("prop-types");
 const Image = require("nordic/image");
+const Card = require("@andes/card");
+const { CardContent } = require("@andes/card");
 
 const ProductsCard = ({ product, i18n, index }) => {
-  const { id, title, name, price, address, thumbnail, installments } = product;
+  const { id, title, price, address, thumbnail, installments } = product;
 
   return (
     <>
-      <li className="card" key={id} role="presentation">
-        <div className="card__image">
-          <figure>
-            <a
-              href={
-                !installments?.quantity || !installments?.amount
-                  ? `/product/${id}`
-                  : `/product/${id}?quantity=${installments.quantity}&amount=${installments.amount}`
-              }
-            >
-              <Image
-                className="card__image__img"
-                src={thumbnail}
-                alt={i18n.gettext("imagen del producto")}
-                tabIndex={`${index + 1}4`}
-              />
-            </a>
-          </figure>
-        </div>
-        <div className="card__description">
-          <h2 className="card__description__title" tabIndex={`${index + 1}3`}>
-            {title || name}
-          </h2>
-          <p
-            className="card__description__price"
-            aria-label={i18n.gettext(`precio del producto: $${price}`)}
-            tabIndex={`${index + 1}5`}
-          >
-            ${price}
-          </p>
-          <span
-            className="card__description__ubication"
-            aria-label={`ubicación del producto ${address ? (address.state_name ? address.state_name : "") : ""
+      <Card>
+        <CardContent className="card" role="presentation">
+          <div className="card__image">
+            <figure>
+              <a
+                href={
+                  !installments?.quantity || !installments?.amount
+                    ? `/product/${id}`
+                    : `/product/${id}?quantity=${installments.quantity}&amount=${installments.amount}`
+                }
+              >
+                <Image
+                  className="card__image__img"
+                  src={thumbnail}
+                  alt={i18n.gettext("imagen del producto")}
+                  tabIndex={`${index + 1}4`}
+                />
+              </a>
+            </figure>
+          </div>
+          <div className="card__description">
+            <div className="card__description__main-info">
+              <p
+                className="price"
+                aria-label={i18n.gettext(`precio del producto: $${price}`)}
+                tabIndex={`${index + 1}5`}
+              >
+                ${price}
+              </p>
+              <h2 className="title" tabIndex={`${index + 1}3`}>
+                {title}
+              </h2>
+            </div>
+            <span
+              className="card__description__ubication"
+              aria-label={`ubicación del producto ${
+                address ? (address.state_name ? address.state_name : "") : ""
               }`}
-            tabIndex={`${index + 1}6`}
-          >
-            {i18n.gettext(
-              address ? (address.state_name ? address.state_name : "") : ""
-            )}
-          </span>
-        </div>
-      </li>
+              tabIndex={`${index + 1}6`}
+            >
+              {i18n.gettext(
+                address ? (address.state_name ? address.state_name : "") : ""
+              )}
+            </span>
+          </div>
+        </CardContent>
+      </Card>
     </>
   );
 };
