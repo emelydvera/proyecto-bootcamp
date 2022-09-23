e60e42f200284ddbd8ff3cf6ffa25eb9a39b2a66
emelydvera <emely.vera@mercadolibre.com.co>
2022-09-23 15:57:09 -0500

    Add test ProductView

--- app/components/ProductView/__tests__/ProductView.spec.js
+++ app/components/ProductView/__tests__/ProductView.spec.js
@@ -1,15 +1,32 @@
-// const ProductView = require("../ProductView")
-// const { I18n } = require("nordic/i18n")
+const React = require('react');
+const { render, screen, fireEvent } = require('@testing-library/react');
+const ProductView = require('../ProductView');
+const { data } = require('../../../../mocks/test/get/https/api.mercadolibre.com/items/MLA1104983845.json')
 
-// const { data } = require('../../../../mocks/test/get/https/api.mercadolibre.com/items/MLA1104983845.json')
 
+describe(' Component <ProductView/>', () => {
 
-// describe(' Component <ProductView/>', () => { 
+    let component;
 
-//     let component;
-//     const props = {
-//         product:data.product
-//         pictures: data.pictures
-//     }
+    const props = {
+        i18n: { gettext: text => text },
+        product: data,
+        description: {
+            plain_text: 'descripcion del producto'
+        },
+        quantity: '852',
+        amount: '1200',
+    }
+    beforeEach(() => {
 
-//  })
+        component = render(<ProductView {...props} />)
+
+    })
+
+    it('should render the components', () => {
+
+        const { asFragment } = component;
+        expect(asFragment()).toMatchSnapshot();
+
+    });
+})
