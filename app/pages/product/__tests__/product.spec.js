9cd22eb29ad7acbc01e6470aff8296125f382232
Ivan E. Batista-Ochoa <ivan.batista@mercadolibre.com.co>
2022-09-23 09:24:42 -0500

    Change mock data and refactor tests

--- app/pages/product/__tests__/product.spec.js
+++ app/pages/product/__tests__/product.spec.js
@@ -1,84 +1,107 @@
 const React = require("react");
 const View = require("../view");
 const { render, screen } = require("@testing-library/react");
-const { getByLabelText, getByText, queryByLabelText, getByRole } = screen;
-const mockProduct = require("../../../../mocks/test/get/https/api.mercadolibre.com/items/MLA1104983845.json");
-const description = require("../../../../mocks/test/get/https/api.mercadolibre.com/items/MLA1104983845/description.json");
+const { getByLabelText, getByText, queryByLabelText, getByRole, getByAltText } =
+  screen;
+const mockProductWithNoEmptyPathFromRoot = require("../../../../mocks/pages/productMockWithNoEmptyPath.json");
+const mockProductWithEmptyPathFromRoot = require("../../../../mocks/pages/productMockWithEmptyPath.json");
+const mockProductDescription = require("../../../../mocks/pages/productDescriptionMock.json");
 const I18nProvider = require("nordic/i18n/I18nProvider");
 
 describe("Product page view", () => {
   const i18n = { gettext: (text) => text, npgettext: (text) => text };
-  let props;
 
-  beforeEach(() => {
-    props = {
-      product: {
-        ...mockProduct.data,
-        path: [],
-      },
-      description: description.data,
-      quantity: "36",
-      amount: "131944.42",
-    };
-  });
-
-  it("should render component", () => {
-    const component = render(
-      <I18nProvider i18n={i18n}>
-        <View {...props} />
-      </I18nProvider>
-    );
-
-    const { asFragment } = component;
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  it("should render component with Breadcrumb and ProductView components when property path in product is not an empty array", () => {
-    const propsWithPath = {
-      ...props,
-      product: {
-        ...mockProduct.data,
-        path: [
-          {
-            id: "MCO1648",
-            name: "Computación",
-          },
-        ],
-      },
+  describe("Product with a non empty path from root array", () => {
+    let component;
+    const props = {
+      product: mockProductWithNoEmptyPathFromRoot,
+      description: mockProductDescription,
+      quantiy: "5",
+      amount: "4300",
     };
 
-    render(
-      <I18nProvider i18n={i18n}>
-        <View {...propsWithPath} />
-      </I18nProvider>
-    );
+    beforeEach(() => {
+      component = render(
+        <I18nProvider i18n={i18n}>
+          <View {...props} />
+        </I18nProvider>
+      );
+    });
 
-    const breadCrumb = getByLabelText("lista de páginas");
-    const productDescriptionTitle = getByText("Descripción del producto");
-    const availableQuantity = getByText(/Cantidad disponible/);
-    const button = getByRole("button");
+    it("should render component", () => {
+      const { asFragment } = component;
+      expect(asFragment()).toMatchSnapshot();
+    });
 
-    expect(breadCrumb).toBeInTheDocument();
-    expect(productDescriptionTitle).toBeInTheDocument();
-    expect(availableQuantity).toBeInTheDocument();
-    expect(button).toHaveTextContent("Comprar");
+    it("should render component with Breadcrumb when property path in product is not an empty array", () => {
+      const breadCrumb = getByLabelText("lista de páginas");
+      expect(breadCrumb).toBeInTheDocument();
+    });
+
+    it("should render product main image", () => {
+      const productMainImage = getByAltText("product");
+      expect(productMainImage).toBeInTheDocument();
+    });
+
+    it("should render product name", () => {
+      const productName = getByRole("heading", {
+        name: /Moto G20 64 Gb Azul Cielo 4 Gb Ram/i,
+      });
+      expect(productName).toHaveTextContent(
+        mockProductWithNoEmptyPathFromRoot.title
+      );
+    });
+
+    it("should render a button to buy (Comprar)", () => {
+      const button = getByRole("button");
+      expect(button).toHaveTextContent("Comprar");
+    });
   });
 
-  it("should render component without Breadcrumb component and with ProductView component when property path in product is an empty array", () => {
-    render(
-      <I18nProvider i18n={i18n}>
-        <View {...props} />
-      </I18nProvider>
-    );
+  describe("Product with an empty path from root array", () => {
+    let component;
+    const props = {
+      product: mockProductWithEmptyPathFromRoot,
+      description: mockProductDescription,
+      quantiy: "5",
+      amount: "4300",
+    };
 
-    const breadCrumb = queryByLabelText("lista de páginas");
-    const productDescriptionTitle = getByText("Descripción del producto");
-    const availableQuantity = getByText(/Cantidad disponible/);
-    const button = getByRole("button");
+    beforeEach(() => {
+      component = render(
+        <I18nProvider i18n={i18n}>
+          <View {...props} />
+        </I18nProvider>
+      );
+    });
 
-    expect(breadCrumb).toBeNull();
-    expect(productDescriptionTitle).toBeInTheDocument();
-    expect(availableQuantity).toBeInTheDocument();
-    expect(button).toHaveTextContent("Comprar");
+    it("should render component", () => {
+      const { asFragment } = component;
+      expect(asFragment()).toMatchSnapshot();
+    });
+
+    it("should render component without Breadcrumb component when property path in product is an empty array", () => {
+      const breadCrumb = queryByLabelText("lista de páginas");
+      expect(breadCrumb).toBeNull();
+    });
+
+    it("should render product main image", () => {
+      const productMainImage = getByAltText("product");
+      expect(productMainImage).toBeInTheDocument();
+    });
+
+    it("should render product name", () => {
+      const productName = getByRole("heading", {
+        name: /Moto G20 64 Gb Azul Cielo 4 Gb Ram/i,
+      });
+      expect(productName).toHaveTextContent(
+        mockProductWithEmptyPathFromRoot.title
+      );
+    });
+
+    it("should render a button to buy (Comprar)", () => {
+      const button = getByRole("button");
+      expect(button).toHaveTextContent("Comprar");
+    });
   });
 });
