5f736e9546326c8c7a75ff3b092b6ed72a90d010
emelydvera <emely.vera@mercadolibre.com.co>
2022-09-23 16:05:46 -0500

    Fix test ProductView

--- app/components/ProductView/__tests__/ProductView.spec.js
+++ app/components/ProductView/__tests__/ProductView.spec.js
@@ -1,5 +1,5 @@
 const React = require('react');
-const { render, screen, fireEvent } = require('@testing-library/react');
+const { render } = require('@testing-library/react');
 const ProductView = require('../ProductView');
 const { data } = require('../../../../mocks/test/get/https/api.mercadolibre.com/items/MLA1104983845.json')
 
@@ -14,7 +14,7 @@ describe(' Component <ProductView/>', () => {
         description: {
             plain_text: 'descripcion del producto'
         },
-        quantity: '852',
+        quantity: '2',
         amount: '1200',
     }
     beforeEach(() => {
