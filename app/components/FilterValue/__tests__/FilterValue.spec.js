dba52a5dc6e7d12cdbe3a795569d2a029dcb1d0d
emelydvera <emely.vera@mercadolibre.com.co>
2022-09-23 15:06:19 -0500

    Add test componente FilterValue

--- app/components/FilterValue/__tests__/FilterValue.spec.js
+++ app/components/FilterValue/__tests__/FilterValue.spec.js
@@ -1,37 +1,29 @@
 const React = require('react');
 const FilterValue = require("../FilterValue");
 const { render, screen, fireEvent } = require('@testing-library/react');
-const { I18n, I18nProvider } = require("nordic/i18n");
 const { getByText } = screen;
 const { data } = require('../../../../mocks/test/get/https/api.mercadolibre.com/sites/MCO/search?limit=10&offset=0&page=1&q=tablet.json');
 const UrlGenerator = require('../../../utils/urlGenerator');
-
-
-const i18n = new I18n({
-    translations: {}
-})
+jest.mock('../../../utils/urlGenerator')
 
 describe('Component <FilterValue/>', () => {
 
     let component;
+    const urlGenerator= new UrlGenerator()
     const props = {
         id: data.available_filters.id,
         value: data.available_filters[0].values[0],
-        urlGenerator: new UrlGenerator()
+        urlGenerator: urlGenerator
     }
 
     beforeEach(() => {
 
-        component = render(
-            <I18nProvider i18n={i18n}>
-                <FilterValue {...props} />
-            </I18nProvider>
-        )
+        component = render(<FilterValue {...props} />)
 
     })
 
     it('should render the components', () => {
-
+        
         const { asFragment } = component;
         expect(asFragment()).toMatchSnapshot();
 
@@ -39,12 +31,9 @@ describe('Component <FilterValue/>', () => {
 
     it('should call setFilter function on click', () => {
 
-        const urlGenerator= new UrlGenerator();
-        const {setFilter } = urlGenerator
-        console.log(urlGenerator);
         const p = getByText(/Todas las tiendas oficiales/i);
         fireEvent.click(p);
-        expect(setFilter).toBeInstanceOf(Function);
+        expect(urlGenerator.setFilter).toHaveBeenCalled();
 
     });
 
