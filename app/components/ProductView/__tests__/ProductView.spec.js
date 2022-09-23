8e7a6d4bb5e5c4ba4cc1f83fda7629f7c57356e5
emelydvera <emely.vera@mercadolibre.com.co>
2022-09-23 11:00:45 -0500

    Add test component ProductView

---
+++ app/components/ProductView/__tests__/ProductView.spec.js
@@ -0,0 +1,15 @@
+// const ProductView = require("../ProductView")
+// const { I18n } = require("nordic/i18n")
+
+// const { data } = require('../../../../mocks/test/get/https/api.mercadolibre.com/items/MLA1104983845.json')
+
+
+// describe(' Component <ProductView/>', () => { 
+
+//     let component;
+//     const props = {
+//         product:data.product
+//         pictures: data.pictures
+//     }
+
+//  })
