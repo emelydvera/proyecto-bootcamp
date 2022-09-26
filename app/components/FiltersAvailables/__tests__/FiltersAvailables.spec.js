d36e5b7b48c7a1882d41f345bd63a92edcea9b70
susanahoriia <suu.ortizm@gmail.com>
2022-09-26 13:35:35 -0300

    started test FiltersAvailables

--- app/components/FiltersAvailables/__tests__/FiltersAvailables.spec.js
+++ app/components/FiltersAvailables/__tests__/FiltersAvailables.spec.js
@@ -4,12 +4,24 @@ const { render, screen, fireEvent } = require("@testing-library/react");
 const { getByText, getByRole, getByPlaceholderText } = screen;
 const {
   data,
-} = require("../../../../mocks/development/get/https/internal-api.mercadolibre.com/sites/MLC/");
+} = require("../../../../mocks/development/get/https/internal-api.mercadolibre.com/sites/MLC/search?limit=10&offset=0&page=1&q=mac.json");
 
+console.log(data.available_filters);
 describe("Component FiltersAvailables", () => {
+  const urlGenerator = jest.fn();
+  let component;
+
   beforeEach(() => {
-    component = render(<FiltersAvailables />);
+    component = render(
+      <FiltersAvailables
+        urlGenerator={urlGenerator}
+        available_filters={data.available_filters}
+      />
+    );
   });
 
-  it("should render correctly", () => {});
+  it("should render correctly", () => {
+    const { asFragment } = component;
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
